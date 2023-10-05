d0c58650d6b26bc06762203e324dd98385b73af3
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-05 22:35:53 +0000

    Task added

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that takes a string and returns a concatenated string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that takes a string and returns a concatenated string"""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that takes a float and returns the floor of the float"""
+
+
+def floor(n: float) -> int:
+    """Function that takes a float and returns the floor of the float"""
+    return int(n)
