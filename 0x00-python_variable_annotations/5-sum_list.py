0322b6a4f209e9bde7defbac1b4c1e0867097531
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-05 22:51:15 +0000

    Complex types

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that takes a list and returns their sum as a float"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns their sum as a float"""
+    return sum(input_list)
