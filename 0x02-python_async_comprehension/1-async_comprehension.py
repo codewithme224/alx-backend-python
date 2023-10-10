ee695edbe8a9330d0f48c41f16c300450938572c
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-10 19:48:08 +0000

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """Async comprehension"""
-
-
-import asyncio
-import random
-from typing import Generator
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Async generator that yields random numbers between 0 and 10"""
-    return [i async for i in async_generator()]
+    return [_ async for _ in async_generator()]
