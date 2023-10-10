8910eadd3588c9cd32ce493e7d8cb9c6ad801145
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-10-10 19:55:16 +0000

    Run time

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Measure the runtime
+"""
+
+
+import asyncio
+from time import perf_counter
+# async_generator = __import__('0-async_generator').async_generator
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+
+async def measure_runtime() -> float:
+    """Measure the total runtime and return it."""
+    start_time = perf_counter()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end_time = perf_counter()
+    return end_time - start_time
