c1c2257a1cba521a56f19109a909b9482ac3503f
codewithme224 <114947924+codewithme224@users.noreply.github.com>
2023-11-03 13:43:13 +0000

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -56,25 +56,22 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    """TestMemoize class
-    """
-    class TestClass:
-        def a_method(self):
-            return 42
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
 
-        @memoize
-        def a_property(self):
-            return self.a_method()
-
-    @patch.object(TestClass, 'a_method')
-    def test_memoize(self, mock_a_method):
-        """Test method for utils.memoize
-        """
-        instance = self.TestClass()
-
-        result1 = instance.a_property()
-        result2 = instance.a_property()
-
-        mock_a_method.assert_called_once()
-        self.assertEqual(result1, 42)
-        self.assertEqual(result2, 42)
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
